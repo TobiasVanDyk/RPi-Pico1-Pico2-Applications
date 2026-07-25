52450e4843a12c9b0418281d936ae1b62f9592fe
5-• 123 •-• <TobiasVanDyk@outlook.com>
2026-07-25 17:13:59 +0200

    Add files via upload

---
+++ TouchMacroPadPico/Capacitive-Touch-LCD/Older/Pico1Source/stringm24.h
@@ -0,0 +1,30 @@
+////////////////
+// stringm24.h
+////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Escape sequences \\ = backslash \? = ? \t = tab \b = backspace \" or \'  \x0A = hexadecimal number (byte)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+char mtr1[] = "Dear Sir or Madam";
+char mtr2[] = "To Whom It May Concern";
+char mtr3[] = "I trust this letter finds you well";                            
+char mtr4[] = "I am writing to inquire about";
+char mtr5[] = "I am submitting this request for";   
+char mtr6[] = "Dear Mr. or Mrs."; 
+char mtr7[] = "I look forward to hearing from you.";                             
+char mtr8[] = "Do not hesitate to get in touch if you need more information";  
+char mtr9[] = "Please keep me informed"; 
+char mtr10[] = "I'd appreciate your quick response";
+char mtr11[] = "Thank you"; 
+char mtr12[] = "Kind regards"; 
+char mtr13[] = "Cordially";
+char mtr14[] = "Best wishes";
+char mtr15[] = "Sincerely";                            
+char mtr16[] = "Thank you for your time";
+char mtr17[] = "Yours truly";  
+char mtr18[] = "Yours faithfully"; 
+char mtr19[] = "Yours sincerely";                             
+char mtr20[] = "Sincerely yours";  
+char mtr21[] = "Best regards";
+char mtr22[] = "Take care"; 
+char mtr23[] = "Farewell";                             
+char mtr24[] = "See you soon"; 
